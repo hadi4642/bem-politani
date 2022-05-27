86ca2339cf807e0f098d7e6f884c7aa30656ff92
Hadi Firmansyah <hadifirmansyah48@gmail.com>
2022-05-28 00:50:06 +0800

    add factory & seeder

---
+++ database/factories/AnggotaFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Anggota>
+ */
+class AnggotaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nim' => $this->faker->unique()->randomNumber(8),
+            'nama' => $this->faker->name,
+            'jenis_kelamin' => $this->faker->randomElement(['Laki-Laki', 'Perempuan']),
+            'no_telp' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'prodi_id' => $this->faker->numberBetween(1, 4),
+            'tahun_angkatan' => $this->faker->year,
+            'divisi_id' => $this->faker->numberBetween(1, 4),
+            'password' => Hash::make('password'),
+            'role' => $this->faker->randomElement(['Admin', 'User']),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Divisi::factory(4)->create();
+        \App\Models\Prodi::factory(4)->create();
+        \App\Models\Anggota::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
